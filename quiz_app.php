bd9f565b8570085f2a763e4f5cd94da13fb8e0a7
Sharmeen <sharmeen.akter.ru@gmail.com>
2024-12-14 20:29:51 +0600

    This is my first change

---
+++ quiz_app.php
@@ -0,0 +1,40 @@
+<?php
+
+function evaluateQuiz(array $questions, array $answers): int{
+    $score = 0;
+    foreach($questions as $index => $question){
+        if($answers[$index] === $question['correct']){
+            $score++;
+        }
+    }
+    return $score;
+}
+
+//Question Array
+$questions = [
+    ['question' => 'What is 2 + 2?', 'correct' => '4'],
+    ['question' => 'What is the capital of France?', 'correct' =>
+    'Paris'],
+    ['question' => 'Who wrote "Hamlet"?', 'correct' =>
+    'Shakespeare'],
+    ];
+//Users answer
+$answers = [];
+
+foreach ($questions as $index => $question) {
+echo ($index + 1) . ". " . $question['question'] . "\n";
+$answers[] = trim(readline("User answer: "));
+}
+
+// score calculation and review
+$score = evaluateQuiz($questions, $answers);
+echo "\nYou  scored $score out of " . count($questions) . ".\n";
+if ($score === count($questions)) {
+echo "Excellent job!\n";
+}
+elseif ($score > 1) {
+echo "Good effort!\n";
+}
+else {
+echo "Better luck next time!\n";
+}
